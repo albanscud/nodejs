d2bf2f0f4a336a763fc4d69264f11c88635a8c30
scud <alban.escudier@hotmail.fr>
2022-11-28 16:54:39 +0100

    rajout

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express')
+const {success} = require('./helper.js')
+let cryptos = require ('./mock-cryptos');
 
 const app = express()
 const port = 3000
 
 app.get('/', (req,res) => res.send('hello, express 3!'))
 
-app.get('/api/cryptos/1', (req,res) => res.send ('Le bitcoin'))
+
+app.get('/api/cryptos', (req,res) => {
+    const message = 'la liste des crypto a bien été trouvée'
+    res.json(success(message, cryptos))
+})
+
+
+
+app.get('/api/cryptos/:id', (req,res) => {
+ const id = parseInt(req.params.id)
+ const crypto = cryptos.find(crypto => crypto.id === id)
+ const message = 'une crypto a bien été trouvée'
+ res.json(success(message, crypto))
+})
+ // nombre total de cryptos
+
+
+
+
 
 app.listen(port, () => console.log(`notre application Node est démarrée sur : http://localhost:${port}`))
