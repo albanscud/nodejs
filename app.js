b7d4d88f41c782325c73e77af6c3d1ab700fe477
scud <alban.escudier@hotmail.fr>
2022-11-30 09:19:16 +0100

    ajout insomnia

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express')
-const {success} = require('./helper.js')
+const morgan = require('morgan')
+const favicon = require('serve-favicon')
+const { success, getUniqueId } = require('./helper.js')
 let cryptos = require ('./mock-cryptos');
 
 const app = express()
 const port = 3000
 
-app.use((req, res, next) => {
-    console.log(`URL : ${req.url}`)
-    next()
-})
+app
+  .use(favicon(__dirname + '/favicon.ico'))
+  .use(morgan('dev'))
 
 app.get('/', (req,res) => res.send('hello, express 3!'))
 
@@ -23,6 +24,14 @@ app.get('/api/cryptos/:id', (req,res) => {
  const crypto = cryptos.find(crypto => crypto.id === id)
  const message = 'une crypto a bien été trouvée'
  res.json(success(message, crypto))
+})
+
+app.post('/api/cryptos', (req, res) => { 
+    const id = getUniqueId(cryptos)
+    const cryptoCreated = { ...req.body, ... {id: id, created :new Date ()}}
+    cryptos.push(cryptoCreated)
+    const message = `une crypto ${cryptoCreated.name} a bien été créée`
+    res.json(success(message, cryptoCreated))
 })
 
 app.listen(port,() => console.log(`notre application est bien demarée sur : http://localhost:${port}`))
--- helper.js
+++ helper.js
@@ -2,3 +2,10 @@ exports.success = (message, data) => {
     return { message, data }
 }
 
+exports.getUniqueId = (cryptos) => {
+    const cryptosIds = cryptos.map(crypto => crypto.id)
+    const maxId = cryptosIds.reduce((a, b) => Math.max(a, b))
+    const uniqueId = maxId + 1
+
+    return uniqueId
+}
